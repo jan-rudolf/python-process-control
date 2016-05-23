1fb183af0df05db09a8a32ae1d8d10f5d847f26d
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2016-05-23 23:06:05 +0200

    Cleaning work with file descriptor.

--- main.py
+++ main.py
@@ -3,40 +3,42 @@ import psutil
 import time
 
 
-def process_dummy_example():
-    f = open("output.txt", "w")
+def process_dummy_example(f):
+	i = 0
 
-    i = 0
+	while i < 10000000:
+		f.write('{}\n'.format(i))
+		i = i + 1
 
-    while i < 10000000:
-        f.write('{}\n'.format(i))
-        i = i + 1
+	return True
 
-    f.close()
+if __name__ == '__main__':
+	f = open("output.txt", "w")
 
+	process_m = multiprocessing.Process(target=process_dummy_example, args=(f,))
+	process_m.start()
 
-process_m = multiprocessing.Process(target=process_dummy_example)
-process_m.start()
+	pid = process_m.pid
 
-pid = process_m.pid
+	print('Process - pid {}, Run for 5s'.format(pid))
 
-print('Process - pid {}, Run 5s'.format(pid))
+	# using http://pythonhosted.org/psutil/#processes
+	process_p = psutil.Process(pid=pid)
 
-# using http://pythonhosted.org/psutil/#processes
-process_p = psutil.Process(pid=pid)
+	time.sleep(5)
 
-time.sleep(5)
+	print(' Suspend/Pause - {}, Wait for 20s'.format(time.strftime("%X")))
+	process_p.suspend()
 
-print(' Suspend/Pause - {}, Wait for 20s'.format(time.strftime("%X")))
-process_p.suspend()
+	time.sleep(20)
 
-time.sleep(20)
+	print(' Resume - {}, Run for 20s'.format(time.strftime("%X")))
+	process_p.resume()
 
-print(' Resume - {}, Run for 20s'.format(time.strftime("%X")))
-process_p.resume()
+	time.sleep(20)
 
-time.sleep(20)
+	print(' Terminate - {}'.format(time.strftime("%X")))
+	process_m.terminate()
+	process_m.join()
 
-print(' Terminate - {}'.format(time.strftime("%X")))
-process_m.terminate()
-process_m.join()
+	f.close()
